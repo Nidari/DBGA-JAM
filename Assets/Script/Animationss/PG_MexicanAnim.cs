de88584e4a271a533aed5034ce92b570e89c12db
dbga_76 <dbga_76@dbga.local>
2017-02-07 22:42:59 +0100

    Created Early Animation System for PG_Mexican

---
+++ Assets/Script/Animationss/PG_MexicanAnim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PG_MexicanAnim : MonoBehaviour {
+
+     Animator MexicanAnim;
+
+	// Use this for initialization
+	void Start () {
+
+        MexicanAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
