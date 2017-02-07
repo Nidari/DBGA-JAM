7c6b1b34d76fd1f796bd9d581e34b4a1d19babf0
dbga_76 <dbga_76@dbga.local>
2017-02-07 22:01:11 +0100

    Imported UI elements and created main menu with working buttons

---
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject MainPanel;
+    public GameObject TutPanel;
+    public GameObject BG;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        }
+
+   public void TutButtonPressed()
+    {
+        TutPanel.SetActive(true);
+        MainPanel.SetActive(false);
+        BG.SetActive(true);
+    }
+   public void backPressed()
+    {
+        TutPanel.SetActive(false);
+        MainPanel.SetActive(true);
+        BG.SetActive(false);
+    }
+
+
+    
+}
