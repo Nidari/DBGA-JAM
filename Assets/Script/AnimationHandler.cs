6897cf5265e9bcd8f8d998600357ca45c9a13341
dbga_37 <dbga_37@dbga.local>
2017-02-08 23:44:17 +0100

    fine giornaa

--- Assets/Script/AnimationHandler.cs
+++ Assets/Script/AnimationHandler.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimationHandler : MonoBehaviour
 {
@@ -8,12 +10,21 @@ public class AnimationHandler : MonoBehaviour
     private GameController gcLinker;
     public Animator gunMexican;
     public Animator gunPeruvian;
+    public Animator pgMexican;
+    public Animator pgPeruvian;
     public float execTimeAnim = 3;
+
+    public Image[] lifesPL1;
+
+    public Image[] lifesPL2;
+
 
     private void Awake()
     {
         gcLinker = FindObjectOfType<GameController>();
-       
+        //lifePanleLinker = GetComponentsInChildren<GameObject>();
+
+
     }
 
     // Update is called once per frame
@@ -35,25 +46,34 @@ public class AnimationHandler : MonoBehaviour
         {
             case BulletType.fire:
                 gunPeruvian.SetBool("triggerFire", true);
-
+                pgMexican.SetBool("triggerHit", true);
+                
                 break;
             case BulletType.doubleFire:
                 gunPeruvian.SetBool("triggerDoubleFire", true);
+                pgMexican.SetBool("triggerHit", true);
                 break;
             case BulletType.heal:
-                //gunMexican.SetBool("triggerFire", true);
+                gunPeruvian.SetBool("triggerFire", true);
+                pgMexican.SetBool("triggerHit", true);
                 break;
             case BulletType.explosion:
                 gunPeruvian.SetBool("triggerExplosion", true);
+                pgMexican.SetBool("triggerHit", true);
+
                 break;
             case BulletType.dodge:
                 gunPeruvian.SetBool("triggerDodge", true);
+                pgMexican.SetBool("triggerDodge", true);
                 break;
             case BulletType.miss:
                 gunPeruvian.SetBool("triggerCilecca", true);
+                pgMexican.SetBool("triggerCilecca", true);
                 break;
             case BulletType.noShoot:
                // gunMexican.SetBool("trigger", true);
+
+                //L'intero tamburo deve ricaricare
                 break;
             default:
                 break;
@@ -63,29 +83,37 @@ public class AnimationHandler : MonoBehaviour
         {
             case BulletType.fire:
                 gunMexican.SetBool("triggerFire", true);
+                pgPeruvian.SetBool("triggerHit", true);
                 break;
             case BulletType.doubleFire:
                 gunMexican.SetBool("triggerDoubleFire", true);
+                pgPeruvian.SetBool("triggerHit", true);
                 break;
             case BulletType.heal:
-              //  gunPeruvian.SetBool("triggerFire", true);
+                gunMexican.SetBool("triggerFire", true);
+                pgPeruvian.SetBool("triggerHit", true);
                 break;
             case BulletType.explosion:
                 gunMexican.SetBool("triggerExplosion", true);
+                pgPeruvian.SetBool("triggerHit", true);
                 break;
             case BulletType.dodge:
                 gunMexican.SetBool("triggerDodge", true);
+                pgPeruvian.SetBool("triggerDodge", true);
                 break;
             case BulletType.miss:
                 gunMexican.SetBool("triggerCilecca", true);
+                pgPeruvian.SetBool("triggerCilecca", true);
                 break;
             case BulletType.noShoot:
                 //gunPeruvian.SetBool("triggerFire", true);
+                //L'intero tamburo deve ricaricare
                 break;
             default:
                 break;
         }
-
+        LifeChangePL1();
+        LifeChangePL2();
         pl1.whichBullet = BulletType.noShoot;
         pl2.whichBullet = BulletType.noShoot;
 
@@ -103,5 +131,29 @@ public class AnimationHandler : MonoBehaviour
         pl2.startButton.gameObject.SetActive(true);
         gcLinker.GamePhase = GameState.InitPhase;
         
+    }
+
+    private void LifeChangePL1()
+    {
+        foreach (var cuori in lifesPL1)
+        {
+            cuori.enabled = false;
+        }
+        //for (int i = 0; i < pl1.playerLife; i++)
+        //{
+        //    lifesPL1[i].enabled = true;
+        //}
+    }
+
+    private void LifeChangePL2()
+    {
+        foreach (var cuori in lifesPL2)
+        {
+            cuori.enabled = false;
+        }
+        //for (int i = 0; i < pl2.playerLife; i++)
+        //{
+        //    lifesPL2[i].enabled = true;
+        //}
     }
 }
