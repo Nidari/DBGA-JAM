21fd2b468b949777225358084bdd464fb4e33300
dbga_22 <dbga_22@dbga.local>
2017-02-08 12:01:22 +0100

    Abstract Bullets Extensions

---
+++ Assets/Script/GamePlay/DamageOne.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DamageOne : AbstractBullet {
+
+
+    public override void Shoot()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
