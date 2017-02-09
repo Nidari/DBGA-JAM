1ebadcbcfa2ee26c849dba8b765ffe4e296901ae
RiTo95 <riccardotorregrossa@outlook.it>
2017-02-09 01:34:27 +0100

    Fatti script per il retry ed il return to main menù

---
+++ Assets/Script/GameOver/GOMenù.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GOMenù : MonoBehaviour {
+
+	AudioSource audioSourceLinker;
+	public  AudioClip audioClipLinker;
+	bool loading = false;
+
+	void Awake()
+	{
+		audioSourceLinker = GameObject.Find("MixerSecondario").GetComponent<AudioSource>();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void StartG()
+	{
+		if (!loading)
+		{
+			StartCoroutine(DeleyedLoad());
+			loading = true;
+		}
+
+
+
+	}
+
+	IEnumerator DeleyedLoad()
+	{
+		audioSourceLinker.PlayOneShot(audioClipLinker);
+
+		yield return new WaitForSeconds(audioClipLinker.length);
+
+		SceneManager.LoadScene(0);
+
+	}
+}
